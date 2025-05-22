2781d61973b9f49c5f7d17d7884886cb0f28b8d7
rnzdoj <rinzin.bhutan.asia@gmail.com>
2025-05-22 10:26:08 +0600

    add dockerignore file

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const http = require('http');
 const PORT = process.env.PORT || 8080;
 http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello World from Kubernetes!\n');
+  res.end('Hello World from Kubernetes! My CI CD is Working\n');
 }).listen(PORT, () => console.log(`Server running on port ${PORT}`));
